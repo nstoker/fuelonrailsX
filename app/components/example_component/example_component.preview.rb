65a947178a1aa6bfd6671057736ab76b5fc9a911
Neil Stoker <neil.stoker@gmail.com>
2023-07-24 19:14:11 +0100

    update gems

---
+++ app/components/example_component/example_component.preview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Describes the componment. Or it would if this wasn't going to be nuked.
+class ExampleComponentPreview < ApplicationViewComponentPreview
+  def default
+    render_component ExampleComponent.new(title: 'Banana')
+  end
+end
---
+++ config/initializers/view_component.rb
@@ -0,0 +1,18 @@
+# frozen_string_component: true
+
+ActiveSupport.on_load(:view_component) do
+  # Extend your preview controller to support authentication and other
+  # application-specific stuff
+  #
+  # Rails.application.config.to_prepare do
+  #   ViewComponentsController.class_eval do
+  #     include Authenticated
+  #   end
+  # end
+  #
+  # Make it possible to store previews in sidecar folders
+  # See https://github.com/palkan/view_component-contrib#organizing-components-or-sidecar-pattern-extended
+
+  ViewComponent::Preview.extend ViewComponentContrib::Preview::Sidecarable
+  # ViewComponent::Preview.extend ViewComponentContrib::Preview::Abstract
+end
