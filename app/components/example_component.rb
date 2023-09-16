3fb746528701d756945ac166a87b9eff679413e7
Neil Stoker <neil.stoker@gmail.com>
2023-09-16 11:39:42 +0100

    remove unused components

--- app/components/example_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ExampleComponent < ApplicationComponent
-  attr_accessor :title
-  def initialize(title:)
-    @title = title
-  end
-end
--- app/components/example_component/example_component.preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Describes the componment. Or it would if this wasn't going to be nuked.
-class ExampleComponentPreview < ApplicationViewComponentPreview
-  def default
-    render_component ExampleComponent.new(title: 'Banana')
-  end
-end
